402cdf21765cdf507b9bed63013c4b959abe1075
dongnguyenvn <nguyenthanhdong997@gmail.com>
2023-01-11 15:27:33 +0700

    pics

---
+++ pics/src/App.tsx
@@ -0,0 +1,154 @@
+import { useState } from "react";
+import { images } from "./data/images";
+import { MotionConfig, motion, AnimatePresence } from "framer-motion";
+import useKeypress from "./hook/useKeyPress";
+
+let collapsedAspectRatio = 1 / 3;
+let fullAspectRatio = 3 / 3;
+let gap = 2;
+let margin = 12;
+
+function App() {
+  const [index, setIndex] = useState<number>(0);
+
+  useKeypress("ArrowRight", () => {
+    if (index + 1 < images.length) {
+      setIndex(index + 1);
+    }
+  });
+
+  useKeypress("ArrowLeft", () => {
+    if (index > 0) {
+      setIndex((i) => i - 1);
+    }
+  });
+  return (
+    <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
+      <div className="h-screen w-screen bg-black flex items-center justify-center">
+        <div className=" max-w-4xl">
+          <div className="relative overflow-hidden">
+            <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
+              {images.map((image) => (
+                <img
+                  key={image.id}
+                  src={image.src}
+                  alt={image.title}
+                  className="aspect-[3/2] object-cover"
+                />
+              ))}
+            </motion.div>
+            <AnimatePresence initial={false}>
+              {index > 0 && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  onClick={() => setIndex(index - 1)}
+                >
+                  <ChevronLeftIcon />
+                </motion.button>
+              )}
+            </AnimatePresence>
+
+            <AnimatePresence initial={false}>
+              {index + 1 < images.length && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  onClick={() => setIndex(index + 1)}
+                >
+                  <ChevronRightIcon />
+                </motion.button>
+              )}
+            </AnimatePresence>
+          </div>
+          <div className="mt-4 flex justify-center overflow-hidden">
+            <motion.div
+              initial={false}
+              animate={{
+                x: `-${
+                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
+                  index * gap +
+                  margin
+                }%`,
+              }}
+              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
+              className="flex h-14"
+            >
+              {images.map((image, i) => (
+                <motion.button
+                  key={image.id}
+                  onClick={() => setIndex(i)}
+                  whileHover={{ opacity: 1 }}
+                  initial={false}
+                  animate={i === index ? "active" : "inactive"}
+                  variants={{
+                    active: {
+                      marginLeft: `${margin}%`,
+                      marginRight: `${margin}%`,
+                      opacity: 1,
+                      aspectRatio: fullAspectRatio,
+                    },
+                    inactive: {
+                      marginLeft: "0%",
+                      marginRight: "0%",
+                      opacity: 0.5,
+                      aspectRatio: collapsedAspectRatio,
+                    },
+                  }}
+                >
+                  <motion.img
+                    src={image.src}
+                    alt={image.title}
+                    className="h-full w-full object-cover"
+                  />
+                </motion.button>
+              ))}
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </MotionConfig>
+  );
+}
+
+const ChevronLeftIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    className="w-4 h-4"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 19.5L8.25 12l7.5-7.5"
+    />
+  </svg>
+);
+
+const ChevronRightIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    className="w-4 h-4"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8.25 4.5l7.5 7.5-7.5 7.5"
+    />
+  </svg>
+);
+
+export default App;
---
+++ pics/src/data/images.ts
@@ -0,0 +1,63 @@
+import Flower_1 from "../assets/flowers/flower_1.jpg";
+import Flower_2 from "../assets/flowers/flower_2.jpg";
+import Flower_3 from "../assets/flowers/flower_3.jpg";
+import Flower_4 from "../assets/flowers/flower_4.jpg";
+import Flower_5 from "../assets/flowers/flower_5.jpg";
+import Flower_6 from "../assets/flowers/flower_6.jpg";
+import Flower_7 from "../assets/flowers/flower_7.jpg";
+import Flower_8 from "../assets/flowers/flower_8.jpg";
+import Flower_9 from "../assets/flowers/flower_9.jpg";
+import Flower_10 from "../assets/flowers/flower_10.jpg";
+
+export const images = [
+  {
+    id: 1,
+    src: Flower_1,
+    title: "Flower 1",
+  },
+  {
+    id: 2,
+    src: Flower_2,
+    title: "Flower 2",
+  },
+  {
+    id: 3,
+    src: Flower_3,
+    title: "Flower 3",
+  },
+  {
+    id: 4,
+    src: Flower_4,
+    title: "Flower 4",
+  },
+  {
+    id: 5,
+    src: Flower_5,
+    title: "Flower 5",
+  },
+  {
+    id: 6,
+    src: Flower_6,
+    title: "Flower 6",
+  },
+  {
+    id: 7,
+    src: Flower_7,
+    title: "Flower 7",
+  },
+  {
+    id: 8,
+    src: Flower_8,
+    title: "Flower 8",
+  },
+  {
+    id: 9,
+    src: Flower_9,
+    title: "Flower 9",
+  },
+  {
+    id: 10,
+    src: Flower_10,
+    title: "Flower 10",
+  },
+];
